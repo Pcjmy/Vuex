33fbda8af9dc2dfcb0c12693eddbe3537868d2b2
Pcjmy <1716045191@qq.com>
2023-02-26 11:50:06 +0800

    feat: 发送axios请求

--- src/views/cart-list.vue
+++ src/views/cart-list.vue
@@ -2,19 +2,43 @@
   <div id="list">
     <h3>商品列表</h3>
     <button>排序</button>
-    <button>跳转购物车</button>
+    <button @click="$router.push('/cart')">跳转购物车</button>
     <ul class="listUl">
-      <div>
-        <img alt="" src="咖啡杯.png" />
-      </div>
-      <div>
-        <p>名称：咖啡杯</p>
-        <p>价格：￥</p>
-        <button>加入购物车</button>
-      </div>
+      <li v-for="item in list" :key="item.id">
+        <div>
+          <img alt="" :src="item.img" />
+        </div>
+        <div>
+          <p>名称：{{ item.title }}</p>
+          <p>价格：￥ {{ item.price }}</p>
+          <button>加入购物车</button>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    // 保存商品列表的数据
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getGoodsList();
+  },
+  methods: {
+    // 获取商品列表数据
+    async getGoodsList() {
+      const { data } = await this.axios.get('/data.json');
+      this.list=data;
+      console.log(this.list);
+    }
+  }
+}
+</script>
 
 <style scoped>
 .listUl {
